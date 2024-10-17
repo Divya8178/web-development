a77434f27217fb0e2c3216e8aab5d10e4c09ef5f
unknown <vishal_249114@saitm.ac.in>
2024-10-17 13:36:47 +0530

    first commit

---
+++ day 3/index.js
@@ -0,0 +1,41 @@
+/*/function changeMessage (){
+  alert("Hello ! this is a winow alert.");
+  
+}
+docyment.getElementByID('alert ')
+.addEventListener('click',changeMessage)*/
+
+
+/*
+document.write("swift");
+   let v = 5 ;
+   v = 8;
+   console.log(v);
+
+   hey =10 ;
+   console.log(typeof(hey))
+
+   const k= 20
+   console.log(k)
+   
+
+ for(var i=0;i<5; i++) {
+    console.log("hello world" + i);
+ }
+ while(count <=5){
+    console.log("count is:" +count);
+    count++;
+ }
+    */
+
+/*let i=1;
+do{
+    console.log(i);
+    i++;
+}while (i <=5);
+
+let numbers =[1,2,3,4,5];
+console.log(numbers[0]);
+console.log(numbers[4]);*/
+  
+   
